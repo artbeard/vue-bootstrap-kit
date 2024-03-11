d6f997519eb18b8e48343533450e415bc5e8a4d3
artbeard <artbeard@internet.ru>
2024-03-11 22:57:42 +0600

    добавлен генератор для создания матрицы календаря

--- src/components/calendar/vi-calendar.vue
+++ src/components/calendar/vi-calendar.vue
@@ -1,4 +1,8 @@
 <template>
+	<pre>{{dateMatrix.length}},{{dateMatrix}}, {{ dateMatrix[35] }}</pre>
+	<div>
+		<span class="px-1" v-for="day in dateMatrix">{{day.getDate()}}</span>
+	</div>
 	<div class="vi-calendar">
 		<div class="vi-weak-control">
 			<div class="vi-day">пн</div>
@@ -9,70 +13,55 @@
 			<div class="vi-day">сб</div>
 			<div class="vi-day">ВС</div>
 		</div>
-		<div class="vi-weak">
-			<div class="vi-day">
-				<div class="vi-day_num">26</div>
-				<div class="vi-day_content">
-					{{getDisplayDate}}
+
+		<template v-for="i in (dateMatrix.length/7 + 1)">
+			<div class="vi-weak">
+				<div class="vi-day" v-for="(day, index) in dateMatrix.slice((i-1) * 7, (i-1) * 7 + 7)" :key="`${day.getDate()}_${day.getMonth()}`">
+					<div class="vi-day_num">{{day.getDate()}}</div>
+					<div class="vi-day_content">
+						{{index}},
+						{{ (i - 1) * 7 }} - {{ (i - 1) * 7 + 7 }}
+
+					</div>
 				</div>
 			</div>
-			<div class="vi-day">
-				<div class="vi-day_num">27</div>
-				<div class="vi-day_content"></div>
-			</div>
-			<div class="vi-day">
-				<div class="vi-day_num">28</div>
-				<div class="vi-day_content"></div>
-			</div>
-			<div class="vi-day">
-				<div class="vi-day_num">29</div>
-				<div class="vi-day_content"></div>
-			</div>
-			<div class="vi-day">
-				<div class="vi-day_num">30</div>
-				<div class="vi-day_content"></div>
-			</div>
-			<div class="vi-day">
-				<div class="vi-day_num">31</div>
-				<div class="vi-day_content"></div>
-			</div>
-			<div class="vi-day">
-				<div class="vi-day_num">1</div>
-				<div class="vi-day_content"></div>
-			</div>
-		</div>
-		<div class="vi-weak">
-			<div class="vi-day">
-				<div class="vi-day_num">26</div>
-				<div class="vi-day_content">
-				</div>
-			</div>
-			<div class="vi-day">
-				<div class="vi-day_num">27</div>
-				<div class="vi-day_content"></div>
-			</div>
-			<div class="vi-day">
-				<div class="vi-day_num">28</div>
-				<div class="vi-day_content"></div>
-			</div>
-			<div class="vi-day">
-				<div class="vi-day_num">29</div>
-				<div class="vi-day_content"></div>
-			</div>
-			<div class="vi-day">
-				<div class="vi-day_num">30</div>
-				<div class="vi-day_content"></div>
-			</div>
-			<div class="vi-day">
-				<div class="vi-day_num">31</div>
-				<div class="vi-day_content"></div>
-			</div>
-			<div class="vi-day">
-				<div class="vi-day_num">1</div>
-				<div class="vi-day_content"></div>
-			</div>
-		</div>
+		</template>
+
+<!--		&#45;&#45;-->
+<!--		<div class="vi-weak">-->
+<!--			<div class="vi-day">-->
+<!--				<div class="vi-day_num">26</div>-->
+<!--				<div class="vi-day_content">-->
+<!--				</div>-->
+<!--			</div>-->
+<!--			<div class="vi-day">-->
+<!--				<div class="vi-day_num">27</div>-->
+<!--				<div class="vi-day_content"></div>-->
+<!--			</div>-->
+<!--			<div class="vi-day">-->
+<!--				<div class="vi-day_num">28</div>-->
+<!--				<div class="vi-day_content"></div>-->
+<!--			</div>-->
+<!--			<div class="vi-day">-->
+<!--				<div class="vi-day_num">29</div>-->
+<!--				<div class="vi-day_content"></div>-->
+<!--			</div>-->
+<!--			<div class="vi-day">-->
+<!--				<div class="vi-day_num">30</div>-->
+<!--				<div class="vi-day_content"></div>-->
+<!--			</div>-->
+<!--			<div class="vi-day">-->
+<!--				<div class="vi-day_num">31</div>-->
+<!--				<div class="vi-day_content"></div>-->
+<!--			</div>-->
+<!--			<div class="vi-day">-->
+<!--				<div class="vi-day_num">1</div>-->
+<!--				<div class="vi-day_content"></div>-->
+<!--			</div>-->
+<!--		</div>-->
+
 	</div>
+
 </template>
 
 <script lang="ts">
@@ -99,18 +88,19 @@ export default defineComponent({
 	watch: {
 		getDisplayDate(newVal, oldVal)
 		{
-			console.log('Пересчитываем матрицу дат для ', newVal);
-			const firstDate = new Date(newVal.getFullYear(), newVal.getMonth(), 1);
-			let rewindDays = (firstDate.getDay() == 0 ? 7 : firstDate.getDay()) - 1;
-			let countDays = 33 - new Date(firstDate.getFullYear(), firstDate.getMonth(), 33).getDate()
-			let countDisplayDays = Math.ceil((rewindDays + countDays) / 7);
-
-
-			firstDate.setDate(firstDate.getDate() - rewindDays);
-			console.log(firstDate.toLocaleDateString(), firstDate.getDay(),
-				countDays,
-				countDisplayDays
-			)
+			// console.log('Пересчитываем матрицу дат для ', newVal);
+			// const firstDate = new Date(newVal.getFullYear(), newVal.getMonth(), 1);
+			// let rewindDays = (firstDate.getDay() == 0 ? 7 : firstDate.getDay()) - 1;
+			// let countDays = 33 - new Date(firstDate.getFullYear(), firstDate.getMonth(), 33).getDate()
+			// let countDisplayDays = Math.ceil((rewindDays + countDays) / 7);
+			//
+			//
+			// firstDate.setDate(firstDate.getDate() - rewindDays);
+			// console.log(firstDate.toLocaleDateString(), firstDate.getDay(),
+			// 	countDays,
+			// 	countDisplayDays
+			// )
+			this.dateMatrix = [...this.generateNewMatrix(newVal)];
 		}
 	},
 
@@ -135,12 +125,23 @@ export default defineComponent({
 	},
 
 	methods: {
-		generateNewMatrix()
+		generateNewMatrix(date: Date)
 		{
+			const firstDate = new Date(date.getFullYear(), date.getMonth(), 1);
+			let rewindDays = (firstDate.getDay() == 0 ? 7 : firstDate.getDay()) - 1;
+			let countDays = 33 - new Date(firstDate.getFullYear(), firstDate.getMonth(), 33).getDate()
+			let countDisplayDays = Math.ceil((rewindDays + countDays) / 7) * 7;
+
+			firstDate.setDate(firstDate.getDate() - rewindDays);
 			function * matrixGenerator(start, count)
 			{
-
+				for (let i = 0; i < count; i++)
+				{
+					start.setDate(start.getDate() + (!!i ? 1 : 0));
+					yield new Date(start.getTime())
+				}
 			}
+			return matrixGenerator(firstDate, countDisplayDays);
 		},
 	},
 
