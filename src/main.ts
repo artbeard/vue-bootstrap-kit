ead0a31d58f63baf76502ddd3a8b683b57a6af8e
artbeard <a.rygovsky@abcage.ru>
2025-01-18 18:20:35 +0600

    update for latest version + clear

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
-import '../node_modules/@tabler/core/dist/css/tabler.css';
-import '../node_modules/@tabler/icons-webfont/tabler-icons.min.css';
+
 
 import './style.scss'
 import App from './App.vue'
